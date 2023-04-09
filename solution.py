b6b3ff331c21b6f1968d67757a9b97a03a041479
squrem <108416846+squrem@users.noreply.github.com>
2023-04-09 17:39:27 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import numpy as np
+from statsmodels.stats.proportion import proportions_ztest
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 434559054 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.04         
+    nobs = np.array([x_cnt, y_cnt])
+    count = np.array([x_success, y_success])
+    res = proportions_ztest(count, nobs, alternative = 'larger')[1]
+    if (res <= alpha): 
+        return True
+    else:
+        return False
